9f5bfd55b964b6ae1184f6c1b90f0f286394a074
GachiBoy0706 <sazhin.dan@gmail.com>
2023-12-10 23:29:57 +0300

    the really last one

--- Wrapper.h
+++ Wrapper.h
@@ -24,10 +24,11 @@ public:
 	Wrapper(ClassType* instance, MethodType method, std::map<std::string, int> arguments):instance(instance), method(method) {
 		for (auto tmp : arguments) 
 			argument_names.push_back(tmp.first);
-		for (auto tmp : argument_names)
-			std::cout << tmp << std::endl;
+		
 	}
 	int operator()(std::unordered_map<std::string, int> arguments_map) {
+		if (arguments_map.size() != argument_names.size())
+			throw std::runtime_error("Incorrect number of argument");
 		for (const auto& argument_tmp : argument_names) {
 			if (arguments_map.find(argument_tmp) == arguments_map.end()) {
 				throw std::runtime_error("Incorrect argument: " + argument_tmp);
